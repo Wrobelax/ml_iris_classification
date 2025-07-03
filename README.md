a05013bb0add974875762333b240061fe095b7ac
Adrian <awrobel.wr@gmail.com>
2025-07-03 19:25:54 +0200

    Continuing data visualization and training models for machine learning.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## **Project description**
 This project utilises iris data from sklearn module, manipulates it, trains ml model and uses for prediction models visualization.
-The data used in the project is pretty simple and covers only 150 cases (rows).
+The data used in the project is pretty simple and covers only 150 cases (rows). Visualization of predictions and trainig/testing data shows close to perfection fit which corresponds with simplicity of the data but also confirm that chosen models properly correspond with the data.
 
 
 ### Features
@@ -25,3 +25,7 @@ The data used in the project is pretty simple and covers only 150 cases (rows).
   * **Conclusion:** 'versicolor' class is confused with 'virginica' sometimes.
 * "conf_matrix_rand_forest.png": Visualization of a confusion matrix for random forest prediction model. It shows where the model is and isn't accurate.
   * **Conclusion:** Logistic regression and random forest confusion matrix are the same. It shows that both work fine on the dataset.
+* "cross_val_lin.png": Linear model for logistic regression and random forest cross-validation 10-fold results.
+  * **Conclusion:** Both models return similar results with small differences. It may be worth to use simpler model which is logistic regression.
+* "class_report_visualization.png": Classification report visualization for logistic regression and random forest.
+  * **Conclusion:** All values are on 1 which means that both models perfectly fit in the data and prediction.
--- iris_model.py
+++ iris_model.py
@@ -1,9 +1,9 @@
 """Main script with the iris model and the data"""
-
+import numpy as np
 # importing modules.
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
@@ -65,17 +65,24 @@ x_test_scaled_df = pd.DataFrame(x_test_scaled, columns = df_train.columns)
 """Training classification model - Logistic regression"""
 
 # Model initialization, training and evaluating accuracy.
-log_reg = LogisticRegression(random_state = 42)
+log_reg = LogisticRegression(max_iter = 200, random_state = 42)
 log_reg.fit(x_train_scaled_df, y_train)
 accuracy = log_reg.score(x_test_scaled_df, y_test)
 # print(f"Accuracy: {accuracy:.2f}") # 1.00
 
 
-# Creating predictions and confusion matrix.
+# Creating predictions, classification report and confusion matrix.
 y_pred = log_reg.predict(x_test_scaled_df)
 cm = confusion_matrix(y_test,y_pred)
-# print(classification_report(y_test, y_pred))
+class_report_lr = classification_report(y_test, y_pred)
+# print(class_report_lr)
 # print(cm)
+
+
+# Creating cross-validation.
+cv_score_lr= cross_val_score(log_reg, df_train, series_named, cv = 10)
+# print(f"Cross-validation accuracy score: {cv_score_lr}")
+# print(f"Mean accuracy: {cv_score_lr.mean()}") # Accuracy: 0.97
 
 
 # Heatmap visualization of a confusion matrix.
@@ -102,11 +109,18 @@ y_pred_rf = rf.predict(x_test_scaled_df)
 # Checking accuracy of the model.
 accuracy_rf = accuracy_score(y_test, y_pred_rf)
 # print(f"Accuracy: {accuracy_rf:.2f}") # 1.00
-# print(classification_report(y_test, y_pred_rf))
 
 
-# Creating confusion matrix.
+# Creating confusion matrix and classification report.
 cm_rf = confusion_matrix(y_test, y_pred_rf)
+class_report_rf = classification_report(y_test, y_pred_rf)
+# print(class_report_rf)
+# print(cm_rf)
+
+# Creating cross-validation.
+cv_score_rf = cross_val_score(rf, df_train, series_named, cv = 10)
+# print(f"Cross-validation accuracy score: {cv_score_rf}")
+# print(f"Mean accuracy: {cv_score_rf.mean()}") # Accuracy: 0.96
 
 
 # Visualization of a confusion matrix.
@@ -117,5 +131,76 @@ sns.heatmap(cm_rf, annot = True, fmt = "d", cmap = "Blues",
 plt.title("Confusion matrix for random forest")
 plt.xlabel("Predicted label")
 plt.ylabel("True label")
-plt.savefig("../ml_iris_classification/data/conf_matrix_rand_forest.png") # Saving the plot to file.
-plt.show()
+# plt.savefig("../ml_iris_classification/data/conf_matrix_rand_forest.png") # Saving the plot to file.
+
+
+
+"""Visualization of a cross-validation for Logistic Regression and Random Forest"""
+plt.figure(figsize = (8, 5))
+x_pos = np.arange(1, 11)
+plt.plot(x_pos, cv_score_lr, marker = "o", label = "Logistic Regression")
+plt.plot(x_pos, cv_score_rf, marker = "s", label = "Random Forest")
+plt.title("Cross-validation accuracy per fold")
+plt.xlabel("Fold")
+plt.ylabel("Accuracy")
+plt.ylim(0.9, 1.1)
+plt.xticks(x_pos)
+plt.grid(True)
+plt.legend()
+# plt.savefig("../ml_iris_classification/data/cross_val_lin.png") # Saving the plot to file.
+
+
+
+"""Visualization of classification reports metrics for Logistic Regression and Random Forest"""
+# Creating variable for unique class names.
+class_names = ["setosa", "versicolor", "virginica"]
+
+
+# Creating dictionary of a report.
+report_lr = classification_report(y_test, y_pred, target_names = class_names, output_dict = True)
+report_rf = classification_report(y_test, y_pred_rf, target_names = class_names, output_dict = True)
+
+classes = class_names
+x_position = np.arange(len(classes))
+width = 0.12
+
+
+# Creating function for extracting reports from reports.
+def get_metrics(report):
+    precision = [report[cls]["precision"] for cls in classes]
+    recall = [report[cls]["recall"] for cls in classes]
+    f1 = [report[cls]["f1-score"] for cls in classes]
+    return precision, recall, f1
+
+prec_lr, rec_lr, f1_lr = get_metrics(report_lr)
+prec_rf, rec_rf, f1_rf = get_metrics(report_rf)
+
+
+# Visualization.
+fig, ax = plt.subplots(figsize = (14, 7))
+
+offsets = {
+    "prec_lr": -2 * width,
+    "rec_lr": -1 * width,
+    "f1_lr": 0,
+    "prec_rf": width,
+    "rec_rf": 2 * width,
+    "f1_rf": 3 * width
+}
+
+ax.bar(x_position + offsets["prec_lr"], prec_lr, width, label = "LogReg Precision", color = "tab:blue", hatch = "//")
+ax.bar(x_position + offsets["rec_lr"], rec_lr, width, label = "LogReg Recall", color = "tab:blue", hatch = "\\\\")
+ax.bar(x_position + offsets["f1_lr"], f1_lr, width, label = "LogReg F1-Score", color = "tab:blue")
+
+ax.bar(x_position + offsets["prec_rf"], prec_rf, width, label = "RandFor Precision", color = "tab:orange", hatch = "//")
+ax.bar(x_position + offsets["rec_rf"], rec_rf, width, label = "RandFor Recall", color = "tab:orange", hatch = "\\\\")
+ax.bar(x_position + offsets["f1_rf"], f1_rf, width, label = "RandFor F1-Score", color = "tab:orange")
+
+ax.set_xticks(x_position)
+ax.set_xticklabels(classes)
+ax.set_ylim(0, 1.1)
+ax.set_title("Comparison of classification metric for Logistic Regression and Random Forest")
+ax.set_ylabel("Score")
+ax.legend(loc = "upper left", bbox_to_anchor = (1,1))
+plt.tight_layout()
+# plt.savefig("../ml_iris_classification/data/class_report_visualization.png") # Saving the plot to file.
